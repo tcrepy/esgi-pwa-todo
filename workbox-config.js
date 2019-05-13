75cfff4858b42823136e1270899a799bbdbeebfd
tcrepy <tanguycrepy@gmail.com>
2019-05-13 18:33:09 +0200

    update

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  "globDirectory": "./",
-  "globPatterns": [
-    "**/*.{css,png,html,js,json,mjs}"
-  ],
-  "swDest": "sw.js"
+    "globDirectory": "./",
+    "importWorkboxFrom": "local",
+    "globIgnores": [
+        "node_modules/**/*",
+        "package*",
+        "workbox-config.js",
+        "images/**/*"
+    ],
+    "runtimeCaching": [{
+        "urlPattern": /\.(?:png|gif|jpg|jpeg|svg)$/,
+        "handler": "CacheFirst",
+        "options": {
+            "cacheName": "image-cache"
+        }
+    }],
+    "globPatterns": [
+        "**/*.{json,jpg,html,js,css}"
+    ],
+    "swDest": "sw.js"
 };
